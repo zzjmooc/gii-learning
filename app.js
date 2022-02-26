83573f36a212b52b3a8907c50bfc01bf5cfe81b4
zzjmooc@163.com <zzjmooc@163.com>
2022-02-26 08:58:32 +0800

    history-1

---
+++ app.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import { ThemeProvider } from 'theme-ui'
+import { theme } from './theme'
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p
+            sx={{
+              color: 'primary',
+              fontFamily: 'heading',
+            }}
+          >
+            Edit
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
